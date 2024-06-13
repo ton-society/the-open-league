758be05eede23b45571f0e91f46756db5ac2a9b6
delovoyhomie <slavik-yakimenko@yandex.ru>
2024-06-13 16:29:27 +0300

    remove ALENKA due low fdv

--- seasons/s4.py
+++ seasons/s4.py
@@ -83,7 +83,6 @@ from projects.tokens.FISH import FISH
 from projects.tokens.DICK import DICK
 from projects.tokens.LLAMA import LLAMA
 from projects.tokens.TONALD import TONALD
-from projects.tokens.ALENKA import ALENKA
 from projects.tokens.WON import WON
 from projects.tokens.HIF import HIF
 from projects.tokens.RANDOM import RANDOM
@@ -159,7 +158,7 @@ S4_tokens = SeasonConfig(
         durev, OPEN, TGRAM, MagicCrystal, TONG, SCALE, GRAM, VIRUS, JVT, BURN, UP,
         GEMSTON, SQD, CATS, RUSD, REDO, Bear, MEM,
         FNZ, SHIP, LAVE, TONK, CES, TON_STARS, SOX, COFE, PEPE, REGI, WIF, ANON,
-        KAKAXA, JETTON, FISH, DICK, LLAMA, TONALD, ALENKA, WON, HIF, RANDOM, INS, SHIT, POE
+        KAKAXA, JETTON, FISH, DICK, LLAMA, TONALD, WON, HIF, RANDOM, INS, SHIT, POE
     ],
     score_model=TokenLeaderboardModelV4()
 )
