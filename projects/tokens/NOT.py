db90a66adf8f662ea1cb1451881ac4c02b9dadee
Pavel Shuvalov <shuvalov.pavel@gmail.com>
2024-06-14 16:56:37 +0400

    apropriate name

--- projects/tokens/NOT.py
+++ projects/tokens/NOT.py
@@ -1,7 +1,7 @@
 from models.project import Token
 
 NOT = Token(
-    name='ANON',
+    name='NOT',
     address='EQAvlWFDxGF2lXm67y4yzC17wYKD9A0guwPkMs1gOsM__NOT',
     decimals=9,
     is_meme=False
