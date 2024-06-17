c4e9a3c3a2bcd1ba3c18fc2a0213f746f3b1b6d2
Aleksandr Kaekhtin <kaehtin.aa@gmail.com>
2024-06-17 16:33:39 +0300

    add Uniton

---
+++ projects/apps/Uniton.py
@@ -0,0 +1,18 @@
+from models.metrics.smc_interaction import SmartContractInteraction
+from models.project import App
+
+"""
+Uniton app
+"""
+
+Uniton = App(
+    name="Uniton",
+    analytics_key="Uniton",
+    url="",
+    metrics=[
+        SmartContractInteraction(
+            "Daily Check-in", 
+            address="EQAzEfZT2kiUeTUHiuGIMgKYOFkWtyO8YZlexzE372Y5PYNv",
+        )
+    ]
+)
