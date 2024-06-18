4a5418dc08da194b83e0a107905c34d9a2765865
Aleksandr Kaekhtin <kaehtin.aa@gmail.com>
2024-06-18 17:10:58 +0300

    fix metrics

--- projects/apps/GramillaWorld.py
+++ projects/apps/GramillaWorld.py
@@ -1,4 +1,5 @@
 from models.metrics.smc_interaction import SmartContractInteraction
+from models.metrics.token_transfer_from_user import TokenTransferFromUser
 from models.project import App
 
 """
@@ -10,9 +11,14 @@ GramillaWorld = App(
     analytics_key="GRMLA_BETA",
     url="https://t.me/gramilla_beta_bo",
     metrics=[
-        SmartContractInteraction(
-            "Application payment", 
-            address="EQD2WYhA6t9LL98JRL63FcKUQDQYD4_y1EU30O_mqG1KbxZU",
+        TokenTransferFromUser(
+            "Application payment",
+            jetton_masters=[
+                "EQC47093oX5Xhb0xuk2lCr2RhS8rj-vul61u4W2UH5ORmG_O" # GRAM
+            ],
+            destinations=[
+                "EQD2WYhA6t9LL98JRL63FcKUQDQYD4_y1EU30O_mqG1KbxZU"
+            ]
         )
     ]
 )
