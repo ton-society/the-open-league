d947ef5d090c7e97e5b9841cc95483264a580fa3
Karim Iskakov <kar.iskakov@gmail.com>
2024-07-09 02:00:02 +0300

    Add GasPump metrics (TODO: check queries with DB connection access)

---
+++ projects/apps/GasPump.py
@@ -0,0 +1,22 @@
+from models.metrics.gaspump_metrics import GasPumpJettonsBuys, GasPumpJettonsSellsAndUnwraps
+from models.project import App
+
+"""
+GasPump app
+"""
+
+GetGems = App(
+    name="GasPump",
+    analytics_key="gaspump",
+    url='https://t.me/gaspump_bot',
+    metrics=[
+        GasPumpJettonsBuys(
+            "GasPump jettons buys",
+            admin_addresses=["EQDM09e4e5kK0CB2vaHsmOmJtRRN-8fn-GUxNLDTjES-oJA2", "EQC24Ilm0hH8O0SUXbyfaLOhpX99E5bB1ZqSLAo6atYEybx5"]
+        ),
+        GasPumpJettonsSellsAndUnwraps(
+            "GasPump jettons sells and unwraps",
+            admin_addresses=["EQDM09e4e5kK0CB2vaHsmOmJtRRN-8fn-GUxNLDTjES-oJA2", "EQC24Ilm0hH8O0SUXbyfaLOhpX99E5bB1ZqSLAo6atYEybx5"]
+        )
+    ]
+)
