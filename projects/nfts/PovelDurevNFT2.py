33cbf32decf57bc95c9cea2b04ee0c2d09e1b096
Slava Yakimenko <92427892+delovoyhomie@users.noreply.github.com>
2024-07-09 22:02:14 +0300

    fix typo

--- projects/nfts/PovelDurevNFT2.py
+++ projects/nfts/PovelDurevNFT2.py
@@ -1,6 +1,6 @@
 from models.project import NFT
 
-PovelDurevNFTNFT = NFT(
+PovelDurevNFT2 = NFT(
     name='Povel Durev NFT #2',
     address='EQCGYlzlIXsUs9lm3LdMcqHicSyl_5QDEn6QR3xdRcjW698K'
     )
