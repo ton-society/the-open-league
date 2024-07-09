e516f12662c3edda6c50075a3ac664c0a46dde6b
delovoyhomie <slavik-yakimenko@yandex.ru>
2024-07-09 22:41:09 +0300

    add new tokens to s5

---
+++ projects/tokens/CAVI.py
@@ -0,0 +1,8 @@
+from models.project import Token
+
+CAVI = Token(
+    name='CAVI',
+    address='EQD6W0raB1aGCr86pWd7-oWpA3QW2VXCJ9smlYcChkIhflt2',
+    decimals=9,
+    url='https://dyor.io/token/EQD6W0raB1aGCr86pWd7-oWpA3QW2VXCJ9smlYcChkIhflt2'
+)
---
+++ projects/tokens/OR.py
@@ -0,0 +1,8 @@
+from models.project import Token
+
+OR = Token(
+    name='OR',
+    address='EQCJLFpI0QR1QA_CYQ77Zm1_n85RDM4yLS46voZGRWbu9sbX',
+    decimals=9,
+    url='https://dyor.io/token/EQCJLFpI0QR1QA_CYQ77Zm1_n85RDM4yLS46voZGRWbu9sbX'
+)
