112fd138695529537d157cca01b39cffd6ba66d9
delovoyhomie <slavik-yakimenko@yandex.ru>
2024-07-11 12:17:43 +0300

    remove boost option

--- projects/tokens/JETTON.py
+++ projects/tokens/JETTON.py
@@ -4,7 +4,7 @@ JETTON = Token(
     name='JETTON',
     address='EQAQXlWJvGbbFfE8F3oS8s87lIgdovS455IsWFaRdmJetTon',
     decimals=9,
-    has_boost=True,
+    has_boost=False,
     boost_link='https://app.ston.fi/pools/EQATnq6W2xv10C19LNlC26xFtTV1fbzMkXmXQCWHqtv6Okf7',
     url='https://coinmarketcap.com/currencies/jetton/'
 )
--- projects/tokens/JVT.py
+++ projects/tokens/JVT.py
@@ -4,7 +4,7 @@ JVT = Token(
     name='JVT',
     address='EQC8FoZMlBcZhZ6Pr9sHGyHzkFv9y2B5X9tN61RvucLRzFZz',
     decimals=9,
-    has_boost=True,
+    has_boost=False,
     boost_link='https://dedust.io/pools/EQB7zhVOQKkfoMtnGU4f_uf0f6HmgcHJj6vZ-NTyHhsaPHCo',
     url='https://dyor.io/token/EQC8FoZMlBcZhZ6Pr9sHGyHzkFv9y2B5X9tN61RvucLRzFZz'
 )
