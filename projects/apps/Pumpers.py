053794dfc20de952858623209e6842a5e77606ff
Aleksandr Kaekhtin <kaehtin.aa@gmail.com>
2024-07-17 10:00:36 +0300

    add descriptions

--- projects/apps/Pumpers.py
+++ projects/apps/Pumpers.py
@@ -11,14 +11,17 @@ Pumpers = App(
     url="https://pumpers.tg/",
     metrics=[
         JettonMasterInteraction(
+            "RequestBuy",
             admin_addresses=["EQBmZ4IuzZc5YJfAJSvXmkD_8I7qnQ5uq7N3U4MtH_N9YLNd"],
             op_codes=[431239495],
         ),
         ProxyContractInteraction(
+            "Buy with UserProxy",
             code_hash="pui2fZF/DpzjMqYOvkjTOR4EDvBVtVyvhCnwRkhkkgM=",
             op_codes=[2122802415],
         ),
         TokenTransferToJettonMaster(
+            "Sell",
             admin_addresses=["EQBmZ4IuzZc5YJfAJSvXmkD_8I7qnQ5uq7N3U4MtH_N9YLNd"],
         ),
     ],
