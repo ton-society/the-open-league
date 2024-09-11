ed002c52484dc43197efa0bbb0a80b1eb3d4de58
Pavel Shuvalov <pavel.s@ton.org>
2024-09-11 14:10:58 +0400

    Stub data for S6 apps

--- seasons/s6.py
+++ seasons/s6.py
@@ -49,77 +49,6 @@ from projects.defi.EVAA import EVAA
 from projects.defi.StonFi import StonFi
 from projects.defi.StormTrade import StormTrade
 from projects.defi.Tradoor import Tradoor
-from projects.tokens.NOT import NOT
-from projects.tokens.PUNK import PUNK
-from projects.tokens.GLINT import GLINT
-from projects.tokens.RECA import RECA
-from projects.tokens.TIGER import TIGER
-from projects.tokens.WALL import WALL
-from projects.tokens.DFC import DFC
-from projects.tokens.MEH import MEH
-from projects.tokens.HYDRA import HYDRA
-from projects.tokens.WEB3 import WEB3
-from projects.tokens.STON import STON
-from projects.tokens.BOLT import BOLT
-from projects.tokens.durev import durev
-from projects.tokens.OPEN import OPEN
-from projects.tokens.TGRAM import TGRAM
-from projects.tokens.MagicCrystal import MagicCrystal
-from projects.tokens.TONG import TONG
-from projects.tokens.SCALE import SCALE
-from projects.tokens.GRAM import GRAM
-from projects.tokens.VIRUS import VIRUS
-from projects.tokens.JVT import JVT
-from projects.tokens.BURN import BURN
-from projects.tokens.UP import UP
-from projects.tokens.GEMSTON import GEMSTON
-from projects.tokens.SQD import SQD
-from projects.tokens.CATS import CATS
-from projects.tokens.RUSD import RUSD
-from projects.tokens.REDO import REDO
-from projects.tokens.Bear import Bear
-from projects.tokens.MEM import MEM
-from projects.tokens.FNZ import FNZ
-from projects.tokens.SHIP import SHIP
-from projects.tokens.LAVE import LAVE
-from projects.tokens.TONK import TONK
-from projects.tokens.CES import CES
-from projects.tokens.TON_STARS import TON_STARS
-from projects.tokens.SOX import SOX
-from projects.tokens.COFE import COFE
-from projects.tokens.PEPE import PEPE
-from projects.tokens.REGI import REGI
-from projects.tokens.WIF import WIF
-from projects.tokens.ANON import ANON
-from projects.tokens.KAKAXA import KAKAXA
-from projects.tokens.JETTON import JETTON
-from projects.tokens.FISH import FISH
-from projects.tokens.DICK import DICK
-from projects.tokens.LLAMA import LLAMA
-from projects.tokens.TONALD import TONALD
-from projects.tokens.MITTENS import MITTENS
-from projects.tokens.WON import WON
-from projects.tokens.HIF import HIF
-from projects.tokens.RANDOM import RANDOM
-from projects.tokens.INS import INS
-from projects.tokens.SHIT import SHIT
-from projects.tokens.POE import POE
-from projects.tokens.DMT import DMT
-from projects.tokens.Spotty import Spotty
-from projects.tokens.PLANE  import PLANE
-from projects.tokens.UTYA import UTYA
-from projects.tokens.M5 import M5
-from projects.tokens.TDOG import TDOG
-from projects.tokens.GLITCH import GLITCH
-from projects.tokens.RUS import RUS
-from projects.tokens.BTC25 import BTC25
-from projects.tokens.MY import MY
-from projects.tokens.OR import OR
-from projects.tokens.MAN import MAN
-from projects.tokens.DYOR import DYOR
-from projects.tokens.CAVI import CAVI
-from projects.tokens.PET import PET
-from projects.tokens.CRYPTON import CRYPTON
 from projects.nfts.TheMinersClubNFTs import TheMinersClubNFTsNFT
 from projects.nfts.YNGEXPLRZ import YNGEXPLRZNFT
 from projects.nfts.Gatto import GattoNFT
@@ -144,10 +73,9 @@ from projects.nfts.TonAlchemists import TonAlchemists
 from projects.nfts.TONSharks import TONSharks
 from projects.nfts.PovelDurevNFT2 import PovelDurevNFT2
 from projects.nfts.TONTanks import TONTanks
-from seasons.app_models import AppLeaderboardModelV2, AppLeaderboardModelV3
+from seasons.app_models import AppLeaderboardModelS6
 from seasons.defi_models import DeFiWeightedRewards
 from seasons.nfts_models import NFTLeaderboardModelV1
-from seasons.tokens_models import TokenLeaderboardModelV4, TokenLeaderboardModelV5
 
 S6_START = 1726138800 # Thu Sep 12 2024 11:00:00 GMT+0000
 S6_END = 1728558000 # Thu Oct 10 2024 11:00:00 GMT+0000
@@ -200,7 +128,8 @@ S6_apps = SeasonConfig(
         Pumpers,
         WowFish,
     ],
-    score_model=AppLeaderboardModelV3()
+    score_model=AppLeaderboardModelS6(),
+    enrollment_sbt="EQAab9xw8NOPvIIxv6wxiDNOGLCLGClA1I4vVUPLqceCh_Z7"
 )
 
 S6_nfts = SeasonConfig(
