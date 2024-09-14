626b5deec49d8347ea91355186b5d2ee30302475
anhhoang2620 <anhhv1@solarapp.asia>
2024-09-14 11:20:54 +0700

    change format address

--- projects/apps/ChickCoop.py
+++ projects/apps/ChickCoop.py
@@ -16,7 +16,7 @@ ChickCoop = App(
                 "EQCkc4zcv43qW8_0D-FD3Br-fxvA2IoQHCSg0o3cguyQ8Nh9",
                 "EQAOsRZKTrMe8ZTtwxxrAC8P84zgmYMF5YML0_NpL0jDrJRq",
                 "EQBUwiwJ5Wczc5QgOo6MKL-XwSaC9z0a46abdLWqRKb5yUt6",
-                "UQDd29ae4JYnoWAo9eFC4B_PJ0_eYoGaG1KAC3F-So-zJE3B"
+                "EQDd29ae4JYnoWAo9eFC4B_PJ0_eYoGaG1KAC3F-So-zJBAE"
             ],
             comment_required=True
         )
