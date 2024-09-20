2e953e2f9c17189c973b1dbba9299ab83d29a471
Aleksandr Kaekhtin <kaehtin.aa@gmail.com>
2024-09-20 16:32:58 +0300

    update TimePrice

--- projects/apps/TimePrice.py
+++ projects/apps/TimePrice.py
@@ -1,4 +1,5 @@
 from models.metrics.dex_swaps import DexSwaps
+from models.metrics.smc_interaction import SmartContractInteraction
 from models.project import App
 from projects.tokens.TIME import TIME
 
@@ -17,6 +18,15 @@ TimePrice = App(
         DexSwaps(
             "Referral swaps",
             "EQCrhMJ2ZpTXE17N-VRyCEUMQphfE-5y1VtkIpmzEPm5xPex",
+        ),
+        DexSwaps(
+            "Referral swaps",
+            "EQC2fgcn74hlpvCxbVdtZ5gTVSOWHXYHVGXbqC9uB5JBQTIa",
+        ),
+        SmartContractInteraction(
+            "SMC Interaction",
+            "EQD_7n1RHphSbYh23FNvHFAIz8JgRVoOBT41BJZugpwZpkoe",
+            comment_required=True,
         ),
     ],
     token=TIME,
