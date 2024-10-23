02c0bb04b0a79ca82fad58164e64d3c814ed0403
smakarov <makarovserhii.work@gmail.com>
2024-10-23 10:03:26 +0300

    change to address to EQ format

--- projects/apps/NobbyGame.py
+++ projects/apps/NobbyGame.py
@@ -27,7 +27,7 @@ NobbyGame = App(
         ),
         SmartContractInteraction(
             "Check-in",
-            "UQDHZ6jIiX3EpFSpMUs7GF9uJP2Vh9FrHaDVxPI5frZvxH0p",
+            "EQDHZ6jIiX3EpFSpMUs7GF9uJP2Vh9FrHaDVxPI5frZvxH0p",
             comment_required=True,
         ),
         NFTActivity(
