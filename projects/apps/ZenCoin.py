0a6f0845bb2d7ebb6e0a263ac2a89f0e5f0a7f39
zencoin <zcyc011@gmail.com>
2024-10-29 15:08:11 +0800

    Update ZenCoin.py

--- projects/apps/ZenCoin.py
+++ projects/apps/ZenCoin.py
@@ -13,5 +13,11 @@ ZenCoin = App(
         SmartContractInteraction(
             "Сheck-in", "EQAKDZF-w4PLAJcXq_RyNysXm1bepKK_OGkDJGeVVe0IlTXs", op_codes=[-926769531]
         ),
+        SmartContractInteraction(
+            "Daily Check-in", "EQAr17Jj72_h8Wier6zl7n8hB5l2RP7R00VdvmWoTCWv-gf6", op_codes=[-926769531, -1351283404]
+        ),
+        SmartContractInteraction(
+        "In-app purchasing", "UQDZMUeSS-OSWCSCsesv8gZwVe_8lpfcq_lIl-B4MzGHLRg9"
+        ),
     ],
 )
