1ebed0d9b00bc383b28f6579c2fcc3e66a64edbf
Chebotov Nickolay <spiritkola@hotmail.com>
2024-11-05 12:36:10 +0300

    Update Nomis.py

--- projects/apps/Nomis.py
+++ projects/apps/Nomis.py
@@ -9,14 +9,26 @@ Nomis = App(
     name="Nomis",
     analytics_key="NomisApp",
     url="https://t.me/NomisAppBot",
-    nfts=["EQBykWvdmoyFD2kB4BJbdrxRqyWKzSLDn-SgwNvznRfbsaKv"],
+    nfts=[
+        "EQBykWvdmoyFD2kB4BJbdrxRqyWKzSLDn-SgwNvznRfbsaKv",
+        "EQB9xB6WbwsLDOAKWBcWZB3DkQqDoxgxpQm7atXkSocNomIs"
+    ],
     metrics=[
+        SmartContractInteraction(
+            "Nomissian NFT", "EQB9xB6WbwsLDOAKWBcWZB3DkQqDoxgxpQm7atXkSocNomIs"
+        ),
         SmartContractInteraction(
             "Interaction", "EQBykWvdmoyFD2kB4BJbdrxRqyWKzSLDn-SgwNvznRfbsaKv"
         ), 
         SmartContractInteraction( 
             "Nomis Boost", 
             "EQBPIFKq_HLbqoksBqPGIMjFjTco2NH7o0ckV5Iocj_A2z-z" 
+        ),
+        NFTActivity(
+            "Project's NFTs", collections=[
+                "EQBykWvdmoyFD2kB4BJbdrxRqyWKzSLDn-SgwNvznRfbsaKv",
+                "EQB9xB6WbwsLDOAKWBcWZB3DkQqDoxgxpQm7atXkSocNomIs"
+            ]
         ),
     ],
 )
