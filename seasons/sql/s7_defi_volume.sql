8f5bae31cc53299bf71200ed0d0dea72f2ee7c21
Aleksandr Kaekhtin <kaehtin.aa@gmail.com>
2024-11-27 09:27:49 +0300

    remove DEX Dimonds

--- seasons/sql/s7_defi_volume.sql
+++ seasons/sql/s7_defi_volume.sql
@@ -9,11 +9,6 @@ with swaps as (
 ), rainbow_points as (
   select address, floor(sum(volume_usd) / 20.) * 1 as points from rainbow_swaps
   group by 1
-), tondiamonds_swaps as (
-  select 'tondiamonds' as project, tx_hash, swap_user as address, volume_usd from swaps where referral_address =upper('0:d9949a2b4a80787112796fc82dd2a4786e42dfcd50d55dee5298c3dc125d1304')
-), tondiamonds_points as (
-  select address, floor(sum(volume_usd) / 20.) * 1 as points from tondiamonds_swaps
-  group by 1
 ), gaspump as (
   select 'gaspump' as project, tx_hash, trader_address as address, ton_amount /1e9 
   * (select price from prices.ton_price where price_ts < event_time order by price_ts desc limit 1)
@@ -84,8 +79,6 @@ with swaps as (
   select distinct address, 1 as degen from tol.enrollment_degen ed 
 ), volume_points as (
   select * from rainbow_points
-  union all
-  select * from tondiamonds_points
   union all
   select * from gaspump_points
   union all
