8c4190e092486cfbf536a136ec3e79e749f70bc4
Aleksandr Kaekhtin <kaehtin.aa@gmail.com>
2024-11-29 21:04:58 +0300

    update links

--- projects/defi/JVault.py
+++ projects/defi/JVault.py
@@ -3,6 +3,6 @@ from models.project import DeFi
 JVault = DeFi(
     name='JVault',
     defillama_slug='jvault',
-    url='https://jvault.xyz/',
+    url='https://jvault.xyz/staking/v2/stake',
     chain='TON-staking'
 )
--- projects/defi/Parraton.py
+++ projects/defi/Parraton.py
@@ -3,5 +3,5 @@ from models.project import DeFi
 Parraton = DeFi(
     name='Parraton',
     defillama_slug='parraton',
-    url='https://t.me/parraton_bot'
+    url='t.me/parraton_bot?startapp=ny'
 )
